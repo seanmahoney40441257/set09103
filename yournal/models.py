a949ff6f33042b1d4a6f072b2f58c7b34746bf16
seanmahoney40441257 <55832161+seanmahoney40441257@users.noreply.github.com>
2019-12-05 13:53:09 +0000

    Update models.py

--- yournal/models.py
+++ yournal/models.py
@@ -26,4 +26,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return "Post('{self.title}', '{self.date_posted}')"
