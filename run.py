dc302a432c0c4a0105a80bfef13c65ba3c98e20c
seanmahoney40441257 <55832161+seanmahoney40441257@users.noreply.github.com>
2019-12-05 12:45:45 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,3 @@
+from yournal import app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ yournal/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ff831df418148239e57a57bfb90e69a5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from yournal import routes
